a67b64e81be8ede8718198c8cdfa3d7c2718810a
zacbfleming <zacbfleming2@gmail.com>
2020-08-16 04:56:03 -0700

    Added new drone signs1.0 signals.1.0
    fine tuned controls for drone.py 
    long run signals will add hand gesture commands for drone
    signs.py to interpret signals

--- signals.py
+++ signals.py
@@ -134,93 +134,28 @@ class Model_X:
         return frame
   
 
-def main():
-    yaw_list = [ ]
-    pitch_list = [ ] 
-    roll_list = [ ]
-    CLIENT=Tello(host='192.168.10.1', retry_count=1)
-    c=('command')
-    try:
-        CLIENT.send_control_command(c, timeout=12)
-    except: pass
-    yaw=0
-    pitch=0
-    roll=0
-    box = [0,0,0,0]
-    data_count = 0
-    frame_count = 0
-    pose_model = '/home/artichoke/A_Tello/work/models/intel/head-pose-estimation-adas-0001/FP32/head-pose-estimation-adas-0001'
-    face_model = '/home/artichoke/A_Tello/work/intel/face-detection-retail-0004/FP32/face-detection-retail-0004'
-    _F = Model_X(face_model)
-    _F.load_model()
-    _P = Model_X(pose_model)
-    _P.load_model()
-    cap = cv2.VideoCapture(0)
-    cap.open(0)
+
+
+class handSignals:        
+        self.model_path =  '/home/artichoke/A_Tello/work/intel/asl-recognition-0004/FP32/asl-recognition-0004'
+        _S = Model_X(model_path)
+        _S.load_model()
     
-    while cap.isOpened():
-        chk, frame2 = cap.read()
-        if not chk:
-            break
-        frame_count+=1
-        h,w,c = np.shape(frame2)
-        frm = np.full_like(frame2, frame2)
-        faceinf = time.time()
-        image = _F.reframe(frame2,300,300,1)
-        box = _F.predict(image,h,w)
-        p_frame = frame2[box[1]:box[3],box[0]:box[2]]
-        face_t = time.time()-faceinf
-        pimage = _P.reframe(p_frame,60,60,1)
-        yaw,pitch,roll = _P.angles(_P.head_predict(pimage))
-        yaw_list.append(yaw)
-        pitch_list.append(pitch)
-        yw=-2*((int(yaw/10))*10)
-        pt=2*((int(pitch/10))*10)
-        frame2 = cv2.rectangle(frame2,(box[0],box[1]),(box[2],box[3]),(0,0,255),2)
-        
-       # cv2.putText(frame2, ('takeoff: %.0f ' % data_count), (50, 300), cv2.FONT_HERSHEY_SIMPLEX, 1,(255, 0,0), 1,cv2.LINE_AA)
-        if abs(yw) < 11 and abs(pt) < 11:
-            data_count+=1
-            
-       # if data_count < 10 and abs(yw) > 11 or data_count<10 and pt > abs(11):
-        #    data_count = 0
-        if data_count == 10:
-            try:
-                #print('takeoff')
-                CLIENT.send_control_command('takeoff')
-            except: pass
-       # time.sleep(1)
-        if data_count > 10:            
-            try:
-                CLIENT.send_rc_control(left_right_velocity=0, forward_backward_velocity=int(pt), up_down_velocity=0,yaw_velocity=int(yw))
-            except: pass
-            
-           # cv2.putText(frame2, ('takeoff: %.0f ' % data_count), (50, 300), cv2.FONT_HERSHEY_SIMPLEX, 1,(255, 0,0), 1,cv2.LINE_AA)
-            cv2.putText(frame2, ('Yaw: %.3f ' % (-yw) ), (400, 150), cv2.FONT_HERSHEY_SIMPLEX, 1,(0, 0, 255), 1,cv2.LINE_AA)
-            cv2.putText(frame2, ('pitch: %.3f ' % pt), (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 1,(0, 0, 255), 1,cv2.LINE_AA)
-     #   cv2.putText(frame2, ('Roll: %.3f ' % roll), (200, 300), cv2.FONT_HERSHEY_SIMPLEX, 1,(0, 255, 0), 1,cv2.LINE_AA)
-            key_pressed=cv2.waitKey(60)
-            cv2.imshow('frame',frame2)
-            if key_pressed == 27:
-            #print(np.std(yaw_list))
-            #print(np.std(pitch_list))
-                CLIENT.send_control_command('land')
-                cv2.waitKey(60)
-                cv2.destroyAllWindows()
-                break
-        
-
-
-
-
-if __name__ == '__main__':
-    main()
-
-
-
-   
     
-   
-
-
-
+    def remake(self,frm, frame_list):
+        imgin  =_S.reframe(frm,224,224,1)
+        if len(frame_list) == 16:
+            img_set = np.asarray(frame_set,dtype=int)
+            sign_set = img_set.reshape(1,3,16,224,224)
+            frame_list = [ ]
+            print('sign_set',_S.predict_sign(sign_set))  
+        if len(frame_list) < 16:
+            frame_list.append(imgin)
+               
+            
+    def decode(key):
+        for j in MASL:
+            if j['label'] == key:
+                print(j['org_text'])
+                tx = j['org_text']
+            return tx
